cfb4681b2f25ef8086f07e0bd1b97d2e6ebc4034
Dipen Shrestha <dipenchrestha111@gmail.com>
2025-01-14 22:10:40 +0545

    modified the footer section

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ function Footer() {
   return (
     <footer className="container mx-auto py-1 fixed bottom-0 md:left-20 bg-white dark:bg-dark-mode">
       <p className="text-xs text-center text-dark-content dark:text-light-content w-full">
-        Designed and Coded by{" "}
+        Designed by{" "}
         <a
           className="font-medium"
           href="https://www.linkedin.com/in/dipen-stha/"
@@ -12,9 +12,6 @@ function Footer() {
         >
           Dipen Shrestha
         </a>{" "}
-        with
-        <span className="text-gradient font-medium"> Love</span> &
-        <span className="text-gradient font-medium"> Coffee</span>
       </p>
     </footer>
   );
