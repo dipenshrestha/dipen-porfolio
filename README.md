96e51a772da475514ba42a92fa1ffd80d8d5c457
Dipen Shrestha <dipenchrestha111@gmail.com>
2025-01-14 21:02:29 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+```javascript
+/ Enter your Personal Details here
+export const personalDetails = {
+  name: "Dipen Shrestha",
+  tagline: "I build things for web",
+  img: profile,
+  about: `Motivated and detail-oriented recent Computer Engineering graduate from Khwopa Engineering College with a strong academic foundation and active volunteer experience. Possess a solid background in programming and design, enhanced by well-honed leadership and communication skills. Passionate about leveraging technical expertise to build a successful career in Professional Services, specializing in innovative programming and design solutions that drive success.`,
+};
+
---
+++ src/Details.js
@@ -0,0 +1,168 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Dipen Shrestha",
+  tagline: "I build things for web",
+  img: profile,
+  about: `Motivated and detail-oriented recent Computer Engineering graduate from Khwopa Engineering College with a strong academic foundation and active volunteer experience. Possess a solid background in programming and design, enhanced by well-honed leadership and communication skills. Passionate about leveraging technical expertise to build a successful career in Professional Services, specializing in innovative programming and design solutions that drive success.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/dipen-stha/",
+  github: "https://github.com/dipenshrestha",
+  twitter: "https://x.com/Dipen_stha1",
+  instagram: "https://www.instagram.com/dipen.sth/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Graphics Designer",
+    Company: `Hult Prize at Khwopa Engineering College`,
+    Location: "Libali, Bhaktapur",
+    Type: "Volunteering Work",
+    Duration: "2021 - 2023",
+  },
+  {
+    Position: "Content Creator",
+    Company: `IT Students of Nepal (ITSNP)`,
+    Location: "Kathmandu",
+    Type: "Volunteering Work",
+    Duration: "Jul 2022 - Dec 2022",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "+2 Science",
+    Company: "Khwopa Secondary School",
+    Location: "Dekocha, Bhaktapur",
+    Type: "Full Time",
+    Duration: "2017-2019",
+  },
+  {
+    Position: "Bachelor in Computer Engineering",
+    Company: `Khwopa Engineering College`,
+    Location: "Libali, Bhaktapur",
+    Type: "Full Time",
+    Duration: "2019 - 2024",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Project title 1",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "dipenchrestha111@gmail.com",
+  phone: "+977 9860744627",
+};
