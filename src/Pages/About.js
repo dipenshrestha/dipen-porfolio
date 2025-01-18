053a15463b64cea8661015c479c532c3d4e14b31
Dipen Shrestha <dipenchrestha111@gmail.com>
2025-01-18 17:35:44 +0545

    added cv download features

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -10,6 +10,10 @@ function About() {
           About Me
         </h1>
         <p className="text-content py-8 lg:max-w-3xl">{personalDetails.about}</p>
+        <p className="text-content lg:max-w-3xl">Click below to download my CV:</p>
+        <a href="./assets/cv/DipenShrestha_resume.pdf" download>
+          <button class="mt-5 mb-10 px-6 py-3 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-600 active:bg-blue-700 disabled:bg-gray-400 transition-transform transform hover:scale-105 active:scale-95">Download CV</button>
+        </a>
       </section>
       <section>
         <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
