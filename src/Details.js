e0b915af06cfa9222ac87e141d5ff2c37a37656e
Dipen Shrestha <dipenchrestha111@gmail.com>
2025-01-18 14:46:57 +0545

    updated personal details

--- src/Details.js
+++ src/Details.js
@@ -36,7 +36,7 @@ export const logos = {
 // Enter your Personal Details here
 export const personalDetails = {
   name: "Dipen Shrestha",
-  tagline: "I build things for web",
+  tagline: "Crafting Web Experiences: Code, Design, Deliver",
   img: profile,
   about: `Motivated and detail-oriented recent Computer Engineering graduate from Khwopa Engineering College with a strong academic foundation and active volunteer experience. Possess a solid background in programming and design, enhanced by well-honed leadership and communication skills. Passionate about leveraging technical expertise to build a successful career in Professional Services, specializing in innovative programming and design solutions that drive success.`,
 };
