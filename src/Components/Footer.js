658807053e045a2e564da41a285f07355287de9d
Dipen Shrestha <dipenchrestha111@gmail.com>
2025-02-11 10:53:30 +0545

    added comments on components

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,12 +3,13 @@ function Footer() {
   return (
     <footer className="container mx-auto py-1 fixed bottom-0 md:left-20 bg-white dark:bg-dark-mode">
       <p className="text-xs text-center text-dark-content dark:text-light-content w-full">
-        Designed by{" "}
+         Designed by{" "} {/* {" "} is used to explicitly add a space in JSX */}
         <a
           className="font-medium"
           href="https://www.linkedin.com/in/dipen-stha/"
-          target="_blank"
-          rel="noreferrer noopener"
+          target="_blank" //open the link in a new tab
+          rel="noreferrer noopener" //noreferrer prevents the new page from knowing the referrer (where the link was clicked from) 
+          // and noopener prevents potential security vulnerabilities where the new tab can manipulate the original page.
         >
           Dipen Shrestha
         </a>{" "}
